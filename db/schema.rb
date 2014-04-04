2444b12274f6934ab824d984d30e673a349e0063
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-04-04 15:43:15 -0400

    playing with howi might implement roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403175649) do
+ActiveRecord::Schema.define(version: 20140404174626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20140403175649) do
     t.datetime "updated_at"
   end
 
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "sessions", force: true do |t|
     t.integer  "user_id"
     t.string   "token"
@@ -71,6 +77,7 @@ ActiveRecord::Schema.define(version: 20140403175649) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "uid"
+    t.integer  "role_id"
   end
 
 end
