b5df56db7f4a1a7b952f5f45a45c4fb5259c88d8
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-04-10 14:28:09 -0400

    Add roles to candidate actions so that  Assessors can't create or access candidate info. Also make the user_aware_controller throw a helpful exception if there isn't a user present.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,15 @@ class Ability
   
   def initialize(user)
      admin_role = Role.find_by_name("Administrator")
-     if user.roles.include? admin_role
-      can :manage, :all  
+     recruiter_role = Role.find_by_name("Recruiter")
+    if user.roles.include? admin_role
+        can :manage, :all
+    elsif user.roles.include? recruiter_role
+        can :manage, Candidate
+        can :create, Submission
+        can :read, :all
     else  
-      can :read, :all  
+        can :read, :all
     end   
   end  
 end 
--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -1,11 +1,43 @@
 require 'spec_helper'
 
+# FIXME: put me into a shared helper class!
+def add_user_without_role_to_session
+  token = '123456789'
+  expiry = Time.now.utc + 20.minutes
+  @user = User.create({ name: 'Bob', email: 'bob@example.com' })
+  Session.create({token: token, token_expiry: expiry, user: @user})
+  @request.headers['Authorization'] = "Bearer #{token}"
+end
+
+def add_user_to_session(role_name)
+  token = '123456789'
+  expiry = Time.now.utc + 20.minutes
+  @role = Role.find_by_name(role_name)
+  @user = User.create({ name: 'Bob', email: 'bob@example.com'})
+  @user.roles.push(@role)
+  Session.create({token: token, token_expiry: expiry, user: @user})
+  @request.headers['Authorization'] = "Bearer #{token}"
+end
+
 describe CandidatesController do
   before(:each) do
     @level = Level.find_by_text('Junior')
   end
 
-  it 'should create a candidate' do
+  it 'should not allow user without role to create a candidate' do
+    add_user_without_role_to_session
+    lambda {post :create, {candidate: {name: 'Bob', email: 'bob@example.com', level_id: @level.id}}}.should raise_exception(CanCan::AccessDenied)
+    expect(Candidate.count).to eql(0)
+  end
+
+  it 'should not allow user with Assessor role to create a candidate' do
+    add_user_to_session('Assessor')
+    lambda {post :create, {candidate: {name: 'Bob', email: 'bob@example.com', level_id: @level.id}}}.should raise_exception(CanCan::AccessDenied)
+    expect(Candidate.count).to eql(0)
+  end
+
+  it 'should allow user with Recruiter role to create a candidate' do
+    add_user_to_session('Recruiter')
     post :create, {candidate: {name: 'Bob', email: 'bob@example.com', level_id: @level.id}}
 
     expect(Candidate.count).to eql(1)
@@ -14,12 +46,38 @@ describe CandidatesController do
     expect(Candidate.last.level.text).to eql(@level.text)
   end
 
-  it 'should list candidates and include level in payload' do
+  it 'should allow user with Administrator role to create a candidate' do
+    add_user_to_session('Administrator')
+    post :create, {candidate: {name: 'Bob', email: 'bob@example.com', level_id: @level.id}}
+
+    expect(Candidate.count).to eql(1)
+    expect(Candidate.last.name).to eql('Bob')
+    expect(Candidate.last.email).to eql('bob@example.com')
+    expect(Candidate.last.level.text).to eql(@level.text)
+  end
+
+  it 'should allow user with Recruiter role to view list of candidates and include level in payload' do
+    add_user_to_session('Recruiter')
     Candidate.create(name: 'Bob', email: 'bob@example.com', level_id: @level.id)
 
     get :index
 
     expect(response.body).to be_json_eql([{name:'Bob', email: 'bob@example.com', level_id: @level.id}].to_json).at_path('candidates')
     expect(response.body).to be_json_eql([{text: @level.text}].to_json).at_path('levels')
+  end
+
+  it 'should allow user with Administrator role to view list of candidates and include level in payload' do
+    add_user_to_session('Administrator')
+    Candidate.create(name: 'Bob', email: 'bob@example.com', level_id: @level.id)
+
+    get :index
+
+    expect(response.body).to be_json_eql([{name:'Bob', email: 'bob@example.com', level_id: @level.id}].to_json).at_path('candidates')
+    expect(response.body).to be_json_eql([{text: @level.text}].to_json).at_path('levels')
+  end
+
+  it 'should not allow user with Assessor role to view list of candidates' do
+    add_user_to_session('Assessor')
+    lambda { get :index }.should raise_exception(CanCan::AccessDenied)
   end
 end
