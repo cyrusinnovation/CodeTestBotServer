4b6450c0e62ae829ac8b031297043ac99c2647ab
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-04-15 16:37:49 -0400

    The order of routes makes a difference. More specific go at the top.

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ CodeTestBotServer::Application.routes.draw do
   resources :assessments, except: [:new, :edit]
   resources :levels, only: [:index]
   resources :languages, only: [:index]
-  resources :roles
-  resources :users
+
   post '/users/assign_role_to_user', to: 'users#assign_role_to_user'
   post '/users/remove_role_from_user', to: 'users#remove_role_from_user'
   get '/users/filter_by_role', to: 'users#filter_by_role'
+  resources :roles
+  resources :users
 
   get '/auth/google/callback', to: 'omniauth_callbacks#google'
   get '/auth/development_token', to: 'omniauth_callbacks#development_token'
