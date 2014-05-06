930d1091186a864bf6e6a6e85fd2fcba648fc31d
Stuart Childs <schilds@cyrusinnovation.com>
2014-05-06 07:31:44 -0400

    Update CORS localhost port to match new client default.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module CodeTestBotServer
 
     config.middleware.use Rack::Cors do
       allow do
-        origins 'localhost:3001'
+        origins 'localhost:4200'
         resource '*',
                  :methods => [:get, :post, :put, :delete, :options],
                  :headers => :any,
