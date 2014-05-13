5bfd10088db65101b3c0956898d2529886cf4d99
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-05-13 12:14:06 -0400

    Should find user with role even if they have other roles too.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
   let(:admin_role) { Role.find_by_name('Administrator') }
+  let(:assessor_role) { Role.find_by_name('Assessor') }
   let(:kate) { User.create({name: 'Kate', email: 'kate@example.com'}) }
   let(:expected_kate) { {email: kate.email, name: kate.name, editable: true, role_ids: []} }
   let(:expected_kate_with_role) { {email: kate.email, name: kate.name, editable: true, role_ids: [admin_role.id]} }
@@ -26,6 +27,23 @@ describe UsersController do
       let(:params) {{role_id: admin_role.id }}
       before { add_user_to_session('Administrator') }
       let(:expected) { [{name: 'Bob', email: 'bob@example.com', editable: true, role_ids: [admin_role.id]}].to_json }
+
+      it { should be_ok }
+      its(:body) { should be_json_eql(expected).at_path('users') }
+    end
+
+    context 'when filtering for a role show user with role even if they have other roles' do
+      let(:params) {{role_id: assessor_role.id }}
+      before {
+        add_user_to_session('Administrator')
+        sue =  User.create({name: 'Sue', email: 'sue@example.com'})
+        sue.roles.push(admin_role)
+        sue.roles.push(assessor_role)
+        sue.save
+      }
+
+      let(:expected) { [{name: 'Sue', email: 'sue@example.com', editable: true, role_ids:
+          [assessor_role.id, admin_role.id]}].to_json }
 
       it { should be_ok }
       its(:body) { should be_json_eql(expected).at_path('users') }
