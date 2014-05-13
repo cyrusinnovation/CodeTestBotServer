1c31439c0422c9e787ab69b58b3245ec6e4445bb
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-05-13 10:47:36 -0400

    Making the user controller more Ember-y. It should set the list of user roles to whatever the update says it is.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,20 @@ class UsersController < UserAwareController
     render :json => User.all
   end
 
+  def show
+    authorize! :view_users, User
+    user_id = params[:id]
+    render :json => User.find(user_id)
+  end
+
+  def update
+    authorize! :view_users, User
+    user_id = params[:id]
+    user = User.find(user_id)
+    updated_user = params[:user]
+    user.roles = updated_user[:role_ids].collect{|role_id| Role.find(role_id)}
+    user.save
+    render :json => user
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,7 @@ describe UsersController do
   let(:admin_role) { Role.find_by_name('Administrator') }
   let(:kate) { User.create({name: 'Kate', email: 'kate@example.com'}) }
   let(:expected_kate) { {email: kate.email, name: kate.name, editable: true, role_ids: []} }
+  let(:expected_kate_with_role) { {email: kate.email, name: kate.name, editable: true, role_ids: [admin_role.id]} }
 
   describe '#index' do
     subject(:response) { get :index }
@@ -18,6 +19,38 @@ describe UsersController do
       it { should be_ok }
       its(:body) { should be_json_eql(expected).at_path('users') }
     end
+  end
+
+  describe '#show' do
+    let(:params) {{ id: kate.id }}
+    subject(:response) {get :show, params}
+    context 'when an admin user looks at kate' do
+      before { add_user_to_session('Administrator') }
+      let(:expected) {{roles: [], user: expected_kate}.to_json}
+      its(:body) {should be_json_eql(expected)}
+    end
+
+    context 'when a recruiter looks at kate' do
+      before { add_user_to_session('Recruiter') }
+      it {should be_forbidden}
+    end
+  end
+
+  describe '#update' do
+    let(:params) {{ id: kate.id, user: expected_kate_with_role }}
+    subject(:response) {post :update, params}
+
+    context 'when an admin user submits an update' do
+      before { add_user_to_session('Administrator') }
+      let(:expected) {{roles: [{name: 'Administrator'}], user: expected_kate_with_role}.to_json}
+      its(:body) {should be_json_eql(expected)}
+    end
+
+    context 'when an assessor submits an update' do
+      before { add_user_to_session('Assessor') }
+      it {should be_forbidden}
+    end
+
   end
 
 end
