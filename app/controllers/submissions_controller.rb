b4683081aaaeb4a50ea19eef7b04fb48b78591b5
Stuart Childs <stuart@childs.io>
2014-05-21 17:18:10 -0400

    Webhook for submissions for fun and profit.
    Hard coded for maximum pain.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class SubmissionsController < UserAwareController
   def create
     authorize! :create, Submission
@@ -12,8 +14,36 @@ class SubmissionsController < UserAwareController
 
     created_submission = Submission.create!(email_text: submission['email_text'], zipfile: file, candidate: candidate, language: language)
     SubmissionMailer.new_submission(created_submission).deliver
+    post_to_webhook(created_submission)
     render :json => created_submission,
            :status => :created
+  end
+
+  def post_to_webhook(submission)
+    url = "#{Figaro.env.app_uri}/submissions/#{submission.id}"
+    msg = "New Code Test Submission: <#{url}|Click to view>"
+    payload = {
+      username: 'code-test-bot',
+      icon_emoji: ':scream:',
+      attachments: [
+        {
+          fallback: msg,
+          pretext: msg,
+          color: '#0000D0',
+          fields: [
+            { title: 'Candidate Level', value: submission.level.text, short: false },
+            { title: 'Language', value: submission.language.name, short: false }
+          ]
+        }
+      ]
+    }
+
+    uri = URI(Figaro.env.slack_webhook)
+    req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
+    req.body = payload.to_json
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    http.request(req)
   end
 
   def index
