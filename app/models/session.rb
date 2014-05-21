03c2a1b22cb882dd91271ab4dec67fa7542c151a
Sam Backus <Samuel.Backus@gmail.com>
2014-05-21 17:37:52 -0400

    whenever the user makes a request to the server, their session expiration time will get reset

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,9 @@ class Session < ActiveRecord::Base
     return false if token_expiry == nil # TODO: Curiosity: setting 0 yield nil
     Time.now.utc >= token_expiry
   end
+  def reset_token_expiry!
+    if token_expiry < (Time.now.utc + 15.minutes)
+      self.token_expiry = Time.now.utc + 15.minutes
+    end
+  end
 end
