2061e7e15e72250a222e71cc0de414da663e8d67
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-05-27 11:28:49 -0400

    An Administrator should be allowed to update a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < UserAwareController
     user_id = params[:id]
     render :json => User.find(user_id)
   end
+
+  def update
+    authorize! :view_users, User
+    user_id = params[:id]
+    user = User.find(user_id)
+    role = Role.find(params[:user][:role_id])
+    user.role = role
+    user.save
+    render :json => user
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe UsersController do
   let(:assessor_role) { Role.find_by_name('Assessor') }
   let(:kate) { User.create({name: 'Kate', email: 'kate@example.com'}) }
   let(:expected_kate) { {email: kate.email, name: kate.name, image_url: nil, editable: true, role_id: nil} }
-  let(:expected_kate_with_role) { {email: kate.email, name: kate.name, image_uri: nil, editable: true, role_id: admin_role.id} }
+  let(:expected_kate_with_role) { {email: kate.email, name: kate.name, image_url: nil, editable: true, role_id: admin_role.id} }
 
   describe '#index' do
     let(:params) {{}}
@@ -37,5 +37,23 @@ describe UsersController do
       before { add_user_to_session('Recruiter') }
       it {should be_forbidden}
     end
+  end
+
+  describe '#update' do
+    subject(:response) {post :update, params}
+
+    context 'when an admin user submits an update' do
+      let(:params) {{ id: kate.id, user: expected_kate_with_role }}
+      before { add_user_to_session('Administrator') }
+      let(:expected) {{roles: [{name: 'Administrator'}], user: expected_kate_with_role}.to_json}
+      its(:body) {should be_json_eql(expected)}
+    end
+
+    context 'when an assessor submits an update' do
+      let(:params) {{ id: kate.id, user: expected_kate_with_role }}
+      before { add_user_to_session('Assessor') }
+      it {should be_forbidden}
+    end
+
   end
 end
