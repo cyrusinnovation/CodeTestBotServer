d7db64b162a85ee2e17406270ed9dff50319e455
Stuart Childs <schilds@cyrusinnovation.com>
2014-06-04 11:36:32 -0400

    Make S3 namespace explicit.

--- lib/aws/s3.rb
+++ lib/aws/s3.rb
@@ -4,7 +4,7 @@ module AWS
   module S3
     def self.setup(access_key, secret_key)
       aws = AWS.setup(access_key, secret_key)
-      Client.new(aws)
+      AWS::S3::Client.new(aws)
     end
   end
 end
