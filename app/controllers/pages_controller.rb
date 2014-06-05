f01b85bd37b7f253b7794e435952558f08dcbf4c
Stuart Childs <schilds@cyrusinnovation.com>
2014-06-05 14:33:57 -0400

    PagesController#update.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
-class PagesController < ApplicationController
+class PagesController < UserAwareController 
   def show
     render :json => Page.find_by_name(params[:id])
+  end
+
+  def update
+    authorize! :update, Page
+    render :json => Page.update_from_json(params[:id], params[:page])
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,7 +7,49 @@ describe PagesController do
 
     subject { get :show, {id: 'test'} }
 
-    it { should be_ok }
-    its(:body) { should be_json_eql(page_json) }
+    it_behaves_like 'a secured route'
+
+    context 'with an active session' do
+      before { add_user_to_session('Assessor') }
+
+      it { should be_ok }
+      its(:body) { should be_json_eql(page_json) }
+    end
+  end
+
+  describe '#update' do
+    let(:page_id) { 'test' }
+    before { Page.create!({name: 'test', raw_text: 'original' }) }
+
+    subject { put :update, { id: page_id, page: { raw_text: 'updated' } } }
+
+    it_behaves_like 'a secured route'
+
+    context 'when user is unauthorized' do
+      before { add_user_to_session('Assessor') }
+
+      it { should be_forbidden }
+    end
+
+    context 'when user is administrator' do
+      before { add_user_to_session('Administrator') }
+
+      context 'when updating an existing page' do
+        let(:expected_json) { { name: 'test', raw_text: 'updated' }.to_json }
+
+        it { should be_ok }
+        its(:body) { should be_json_eql(expected_json).at_path('page') }
+        it 'should update the page' do
+          subject
+
+          expect(Page.find_by_name(page_id).raw_text).to eq('updated')
+        end
+      end
+
+      context 'when update a page that does not exist' do
+        let(:page_id) { 'not_existing' }
+        it { should be_not_found }
+      end
+    end
   end
 end
