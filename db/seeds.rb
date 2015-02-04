c92a44e3ea17d608268501cf8540902c532dc286
Michael Stricklen <michael.stricklen@gmail.com>
2015-02-04 10:54:40 -0500

    add scala, clojure, python as submission langs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 levels = Level.create(['Junior', 'Mid', 'Senior', 'Tech Lead'].collect_concat {|l| {text: l}})
-languages = Language.create([{name: 'Java'}, {name: 'Ruby'}])
+languages = Language.create([{name: 'Java'}, {name: 'Ruby'}, {name: 'Scala'}, {name: 'Python'}, {name: 'Clojure'}])
 roles = Role.create([{name: 'Assessor'}, {name: 'Recruiter'}, {name: 'Administrator'}])
