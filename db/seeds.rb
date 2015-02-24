af983e45006cf7c78656fc9b2b460e5868190b29
Charles Quirin <crq@users.noreply.github.com>
2015-02-24 16:21:04 -0500

    Fixed test for language controller to reflect addition of new languages in seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-levels = Level.create(['Junior', 'Mid', 'Senior', 'Tech Lead'].collect_concat {|l| {text: l}})
-languages = Language.create([{name: 'Java'}, {name: 'Ruby'}, {name: 'Scala'}, {name: 'Python'}, {name: 'Clojure'}])
+levels = Level.create(['Junior', 'Mid', 'Senior', 'Tech Lead']
+                .collect_concat { |l| {text: l}})
+
+languages = Language.create([{name: 'Java'}, {name: 'Ruby'}, {name: 'Scala'},
+                             {name: 'Python'}, {name: 'Clojure'}])
+
 roles = Role.create([{name: 'Assessor'}, {name: 'Recruiter'}, {name: 'Administrator'}])
--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -8,7 +8,7 @@ describe LanguagesController do
 
     context 'with an active session' do
       before { add_user_to_session('Assessor') }
-      let(:expected) { [{name: 'Java'}, {name: 'Ruby'}].to_json }
+      let(:expected) { [{name: 'Java'}, {name: 'Ruby'}, {name: 'Scala'}, {name: 'Python'}, {name: 'Clojure'}].to_json }
 
       it { should be_ok }
       its(:body) { should be_json_eql(expected).at_path('languages') }
