178862f27715e92c6af1bc5fa0ccc51b064a3336
Mark Simpson <verdammelt@gmail.com>
2015-04-10 10:43:58 -0400

    Uploading and attaching resumefile.
    Mirrors the same actions for zipfiles.

--- app/services/submission_creator.rb
+++ app/services/submission_creator.rb
@@ -1,9 +1,19 @@
 class SubmissionCreator
   def self.create_submission(submission)
     created_submission = Submission.create_from_json(submission)
-    file_name = submission[:zipfile_name]
-    file_url = SubmissionFileUploader.upload(created_submission, submission[:zipfile], file_name)
-    created_submission.attach_zipfile(file_url)
+
+    zipfile_name = submission[:zipfile_name]
+    zipfile_url = SubmissionFileUploader.upload(created_submission,
+                                                submission[:zipfile],
+                                                zipfile_name)
+    created_submission.attach_zipfile(zipfile_url)
+
+    resumefile_name = submission[:resumefile_name]
+    resumefile_url = SubmissionFileUploader.upload(created_submission,
+                                                   submission[:resumefile],
+                                                   resumefile_name)
+    created_submission.attach_resumefile(resumefile_url)
+
     Notifications::Submissions.new_submission(created_submission)
     created_submission
   end
--- spec/services/submission_creator_spec.rb
+++ spec/services/submission_creator_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe SubmissionCreator do
     let(:file_url) { '/some/url' }
-    let(:submission_json) { { zipfile: 'header,====', zipfile_name: 'filename.zip' } }
+    let(:submission_json) { {
+                              zipfile: 'header,====',
+                              zipfile_name: 'filename.zip',
+                              resumefile: 'header,+++++',
+                              resumefile_name: 'resumefile.pdf'
+                            } }
     let(:submission) { Submission.new({ id: 5}) }
 
     before {
@@ -10,20 +15,23 @@ describe SubmissionCreator do
       SubmissionFileUploader.stub(:upload => file_url)
       Notifications::Submissions.stub(:new_submission)
       submission.stub(:attach_zipfile)
+      submission.stub(:attach_resumefile)
     }
 
     it 'creates and returns a submission' do
       expect(SubmissionCreator.create_submission(submission_json)).to eql submission
     end
 
-    it 'uploads the file' do
+    it 'uploads the zipfile and attaches to submission' do
       SubmissionCreator.create_submission(submission_json)
       expect(SubmissionFileUploader).to have_received(:upload).with(submission, submission_json[:zipfile], 'filename.zip')
+      expect(submission).to have_received(:attach_zipfile).with(file_url)
     end
 
-    it 'attaches the uploaded file to the submission' do
+    it 'uploads the resumefile and attaches to submission' do
       SubmissionCreator.create_submission(submission_json)
-      expect(submission).to have_received(:attach_zipfile).with(file_url)
+      expect(SubmissionFileUploader).to have_received(:upload).with(submission, submission_json[:resumefile], 'resumefile.pdf')
+      expect(submission).to have_received(:attach_resumefile).with(file_url)
     end
 
     it 'should send notifications' do
