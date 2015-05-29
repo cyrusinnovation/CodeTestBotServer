20f81013c78007e920d052484496bdbd167b8716
Ben Karl <bkarl@cyrusinnovation.com>
2015-05-29 17:33:30 -0400

    make ApplicationHelper accessible in spec_helper to enable the markdown_to_html method in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,7 @@ include CodeTestBotServer::Helpers
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include ApplicationHelper
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
